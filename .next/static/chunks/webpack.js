6a1bacec625bf5f97dbe4059daebd024ff21835a
PedroNnsM <pedronunes95@outlook.com>
2022-06-05 09:41:29 -0300

    ok

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4bdca7aa5a1e71c2"; }
+/******/ 		__webpack_require__.h = function() { return "3961942710008977"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
