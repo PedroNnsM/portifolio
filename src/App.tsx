f652f05ad3a431698b4295fa4e23a16e0283b4b1
PedroNnsM <pedronunes95@outlook.com>
2025-01-07 04:59:30 -0700

    Portifolio component done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Slider } from "./components/carousel/Slider";
-import { SwiperSlide } from "swiper/react";
-import { BsGithub } from 'react-icons/bs';
+
 import { Header } from "./components/Header";
 import { SobreMim } from "./components/SobreMim";
 import "./components/carousel/Slider.css";
 
-import memoria from "./assets/memoria.svg";
 import { Banner } from "./components/Banner";
 import { Tecnologias } from "./components/Tecnologias/Index";
-
-
+import { Portifolio } from "./components/Portifolio";
 
 type Repository = {
   id: number;
@@ -32,26 +28,6 @@ type SliderSettings = {
 
 export function App() {
   const [show, setShow] = useState<boolean>(true);
-  const [repositories, setRepositories] = useState<Repository[]>([]);
-
-  useEffect(() => {
-    fetch("https://api.github.com/users/PedroNnsM/repos")
-      .then((response) => response.json())
-      .then((data: Repository[]) => setRepositories(data));
-  }, []);
-
-  const repositoriesFilter = repositories.filter(
-    (repository) => repository.homepage === null
-  );
-
-  const settings: SliderSettings = {
-    slidesPerView: 3,
-    spaceBetween: 50,
-    navigation: true,
-    pagination: {
-      clickable: true,
-    },
-  };
 
   window.addEventListener("scroll", function () {
     const header = document.querySelector("header");
@@ -63,61 +39,10 @@ export function App() {
   return (
     <>
       <Header />
-
-     <Banner />
-
-     <SobreMim show={show} setShow={setShow} />
-
+      <Banner />
+      <SobreMim show={show} setShow={setShow} />
       <Tecnologias />
-
-      <section className="sec work" id="portifolio">
-        <div className="content">
-          <h3>Projetos Recentes</h3>
-          <div className="workBx">
-            {repositories.map((repository) => (
-              <div className="brand" key={repository.id}>
-                <a
-                  href={repository.html_url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={memoria} alt="Projeto" />
-                  <span>
-                    <p>{repository.description}</p>
-                    Tecnologias: {repository.language}
-                  </span>
-                  <h2>{repository.name}</h2>
-                </a>
-              </div>
-            ))}
-          </div>
-          <Slider settings={settings}>
-            {repositories.map((repository) => (
-              <SwiperSlide className="teste" key={repository.id}>
-                <span>
-                  <p>{repository.description}</p>
-                  <a
-                    href={repository.homepage ?? undefined}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <button>Demo</button>
-                  </a>
-                  <a
-                    href={repository.html_url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <BsGithub /> Github
-                  </a>
-                </span>
-                <h2>{repository.name}</h2>
-              </SwiperSlide>
-            ))}
-          </Slider>
-        </div>
-      </section>
-     
+      <Portifolio />
     </>
   );
 }
