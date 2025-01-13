8e3ab89999be11b50566ef9bbf1d32f80b15b950
PedroNnsM <pedronunes95@outlook.com>
2025-01-13 05:20:43 -0700

    tailwind extends

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,17 @@ module.exports = {
       backgroundImage: {
         "banner-bg": "url('./src/assets/bg.jpg')",
       },
+      fontFamily: {
+        sans: ['"Josefin Sans"', "sans-serif"],
+        rubik: ['"Rubik"', "sans-serif"],
+        acme: ['"Acme"', "sans-serif"],
+        marker: ['"Permanent Marker"', "cursive"],
+      },
+    },
+    colors: {
+      bluePrimary: "#0d6ba6",
+      blueLight: "#4978ff",
+      blackDark: "#0d0d0d",
     },
   },
   plugins: [],
