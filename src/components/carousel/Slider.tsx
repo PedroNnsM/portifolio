0e23e604ffd4668fddb4496c09f9271d7da79b3b
PedroNnsM <pedronunes95@outlook.com>
2025-01-12 19:18:53 -0700

    tailwind slider

--- src/components/carousel/Slider.tsx
+++ src/components/carousel/Slider.tsx
@@ -1,11 +1,11 @@
 import { Swiper, SwiperProps } from "swiper/react";
 import { ReactNode } from "react";
 import { Navigation, Pagination, A11y, EffectCards } from "swiper";
-import React from "react";
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import React from "react";
 
 interface SliderProps {
   settings: SwiperProps;
@@ -14,8 +14,23 @@ interface SliderProps {
 
 export function Slider({ settings, children }: SliderProps) {
   return (
-    <Swiper modules={[Navigation, Pagination, A11y, EffectCards]} {...settings}>
+    <Swiper
+      modules={[Navigation, Pagination, A11y, EffectCards]}
+      {...settings}
+      className="w-full px-[50px] pb-[50px]" // Classe do swiper
+    >
       {children}
     </Swiper>
   );
 }
+
+// Exemplos de uso:
+const slides = [
+  <div
+    className="flex justify-center items-center w-full h-[250px] bg-blue-600 rounded-[10px] shadow-[0px_15px_30px_rgba(0,0,0,0.5)] hover:shadow-[0px_15px_30px_rgba(0,0,0,0.6)] hover:bg-[#024873]"
+    key={1}
+  >
+    <p className="text-left text-white">Slide 1</p>
+  </div>,
+  <div className="...">Slide 2</div>,
+];
